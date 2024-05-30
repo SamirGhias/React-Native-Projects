b2e8296ab15383f0bfdd51b9b9bc9b2103f27a09
SamirGhias <samir.ghias99@gmail.com>
2024-05-30 15:09:49 -0400

    pushed complete game

--- Mini Game App/App.js
+++ Mini Game App/App.js
@@ -1,15 +1,33 @@
-import { StyleSheet, View, ImageBackground, SafeAreaView } from 'react-native';
+import { StyleSheet, View, ImageBackground, StatusBar } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import GameScreen from './screens/GameScreen';
 import Colors from './constants/colors';
+import GameOverScreen from './screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
   function pickedNumberHandler(pickedNumber) {
     setUserNumber(pickedNumber);
+    setGameIsOver(false);
     console.log(pickedNumber + ' Is the new number!');
+  }
+
+  function gameOverHandler() {
+    setGameIsOver(true);
+  }
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+  function addRoundHandler() {
+    setGuessRounds((prev) => prev + 1);
   }
 
   let screen = (
@@ -17,8 +35,25 @@ export default function App() {
   );
 
   if (userNumber) {
-    screen = <GameScreen></GameScreen>;
+    screen = (
+      <GameScreen
+        userNumber={userNumber}
+        onGameOver={gameOverHandler}
+        onNextRound={addRoundHandler}
+      ></GameScreen>
+    );
   }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      ></GameOverScreen>
+    );
+  }
+
   return (
     <LinearGradient
       colors={[Colors.primary700, Colors.accent500]}
@@ -40,6 +75,10 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
     // backgroundColor: '#ddb52f',
+  },
+  statusbar: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
   },
   backgroundImage: {
     opacity: 0.15,
--- Mini Game App/screens/GameScreen.js
+++ Mini Game App/screens/GameScreen.js
@@ -1,7 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, FlatList, StyleSheet, Text, View } from 'react-native';
 import Title from '../components/ui/Title';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
+import Card from '../components/ui/Card';
+import InstructionText from '../components/ui/InstructionText';
+import { Ionicons } from '@expo/vector-icons';
+import GuessLogItem from '../components/game/GuessLogItem';
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -12,19 +17,91 @@ function generateRandomBetween(min, max, exclude) {
     return rndNum;
   }
 }
+let minBoundary = 1;
+let maxBoundary = 100;
 
-export default function GameScreen({ userNumber }) {
+export default function GameScreen({ userNumber, onGameOver, onNextRound }) {
   initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  const [guessRounds, setGuessRounds] = useState([]);
+
+  useEffect(() => {
+    if (userNumber === currentGuess) {
+      onGameOver();
+    }
+  }, [userNumber, currentGuess, onGameOver]);
+
+  useEffect(() => {
+    minBoundary = 1;
+    maxBoundary = 100;
+  }, []);
+
+  function nextGuessHandler(direction) {
+    // console.log('lowest: ' + minBoundary + ', highest: ' + maxBoundary);
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'higher' && currentGuess > userNumber)
+    ) {
+      Alert.alert(
+        'Misleading Input!',
+        'Enter the correct direction please...',
+        [{ text: 'Sorry!', style: 'cancel' }]
+      );
+      return;
+    }
+
+    if (direction == 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+    setGuessRounds((prevRounds) => [newRndNumber, ...prevRounds]);
+    onNextRound();
+  }
+
+  const guessRoundsListLength = guessRounds.length;
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <View>
-        <Text>Higher or Lower?</Text>
-        {/* + - */}
-      </View>
-      {/* <View>Log Rounds</View> */}
+      <Card>
+        <InstructionText style={styles.instructionText}>
+          Higher or Lower?
+        </InstructionText>
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'higher')}>
+              <Ionicons name='add' size={24}></Ionicons>
+            </PrimaryButton>
+          </View>
+          <View style={styles.buttonContainer}>
+            <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+              <Ionicons name='remove' size={24}></Ionicons>
+            </PrimaryButton>
+          </View>
+        </View>
+      </Card>
+      {/* <View>
+        {guessRounds.map((round) => (
+          <Text key={round}>{round}</Text>
+        ))}
+      </View> */}
+      <FlatList
+        data={guessRounds}
+        renderItem={(item) => (
+          <GuessLogItem
+            roundNumber={guessRoundsListLength - item.index}
+            guess={item.item}
+          />
+        )}
+        keyExtractor={(item) => item}
+      ></FlatList>
     </View>
   );
 }
@@ -42,5 +119,14 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: '#ddb52f',
     padding: 12,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  instructionText: {
+    marginBottom: 12,
   },
 });
